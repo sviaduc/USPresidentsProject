eca1aa573723220ebcdda754acd7b5ecc1a7fe2a
Brian Thomas <brianraythomas@gmail.com>
2017-09-09 10:16:05 -0600

    basic .jsp page working

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+home.jsp page showing
 </body>
 </html>
--- src/com/sd/web/PresServlet.java
+++ src/com/sd/web/PresServlet.java
@@ -1,7 +1,17 @@
 package com.sd.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class PresServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/home.jsp").forward(req, resp);
+	}
 
 }
