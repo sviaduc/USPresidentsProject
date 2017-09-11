286c7c72c428ec60b1151019ed961ed3cfa20525
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-09-11 17:22:39 -0600

    updated for split sessions

--- src/com/sd/web/PresServlet.java
+++ src/com/sd/web/PresServlet.java
@@ -6,46 +6,61 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class PresServlet extends HttpServlet {
-	private PresidentDAO presidentDAO;
-	static int presNum;
-	
-	@Override
-	public void init() throws ServletException {
-		presidentDAO = new PresidentFileDAO(getServletContext());
-	}
+    private PresidentDAO presidentDAO;
+//    private Integer presNum;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setAttribute("presidents", presidentDAO.getPresList());
-		req.setAttribute("currentPres", presidentDAO.getPresList().get(0));
-		req.getRequestDispatcher("/home.jsp").forward(req, resp);
-	}
+    @Override
+    public void init() throws ServletException {
+        presidentDAO = new PresidentFileDAO(getServletContext());
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String next =req.getParameter("next");
-		String submit = req.getParameter("submit");
-		int num = 1;
-		if(req.getParameter("Number") != null) {
-			num = Integer.parseInt(req.getParameter("Number"));
-		}
-		if(next != null && next.equals("Next")) {
-		if (presNum <= presidentDAO.getPresList().size() - 1) {
-			presNum++;
-		}
-		}
-		if(next != null && next.equals("Previous"))
-		if (presNum != 0) {
-			presNum--;
-		}
-		if(submit != null && submit.equals("Submit")) {
-			presNum = num - 1;
-		}
-		req.setAttribute("currentPres", presidentDAO.getPresList().get(presNum));
-		req.setAttribute("numPres", presidentDAO.getPresList().size());
-		req.getRequestDispatcher("/home.jsp").forward(req, resp);
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("presidents", presidentDAO.getPresList());
+        req.setAttribute("currentPres", presidentDAO.getPresList().get(0));
+        req.getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer presNum = (Integer) session.getAttribute("presNum");
+        if (presNum == null) {
+            presNum = 0;
+//            session.setAttribute("presNum", presNum);
+        }
+
+        String next = req.getParameter("next");
+        String submit = req.getParameter("submit");
+
+        // Integer num = (Integer)session.getAttribute("num");
+        int num = 1;
+        
+        if (req.getParameter("Number") != null) {
+            num = Integer.parseInt(req.getParameter("Number"));
+        }
+
+        if (next != null && next.equals("Next")) {
+            if (presNum <= presidentDAO.getPresList().size() - 1) {
+                presNum++;
+            }
+        }
+
+        if (next != null && next.equals("Previous")) {
+            if (presNum != 0) {
+                presNum--;
+            }
+        }
+        if (submit != null && submit.equals("Submit")) {
+            presNum = num - 1;
+        }
+        session.setAttribute("presNum", presNum);
+        req.setAttribute("currentPres", presidentDAO.getPresList().get(presNum));
+        req.setAttribute("numPres", presidentDAO.getPresList().size());
+        req.getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
 
 }
